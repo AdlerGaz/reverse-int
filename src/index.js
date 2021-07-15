f2efd91f50d0feb4a7a1497d437964868cd5216e
Yury Maniakhin <manah9191@mail.ru>
2021-07-15 18:34:38 +0300

    feat: function revers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    let temp = (Math.abs(n)).toString().split('');
+    let result = '';
+    for (let i = temp.length - 1; i >= 0; i--) {
+        result += temp[i];
+    }
+    return +result;
 }
